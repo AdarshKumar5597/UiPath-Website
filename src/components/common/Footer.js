a699752078969920bb6834d5e27b4d900c765543
hritesh04 <hritesh04.dev@gmail.com>
2024-01-08 20:01:33 +0530

    layout height fix

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import { Footer } from 'flowbite-react';
-import { FaBuilding, FaUserTie, FaFileAlt, FaNewspaper, FaDiscord, FaTwitter, FaFacebook, FaPhoneAlt, FaDownload, FaApple, FaAndroid, FaWindows, FaApplePay, FaInstagram } from 'react-icons/fa';
+import React from "react";
+import { Footer } from "flowbite-react";
+import {
+  FaBuilding,
+  FaUserTie,
+  FaFileAlt,
+  FaNewspaper,
+  FaDiscord,
+  FaTwitter,
+  FaFacebook,
+  FaPhoneAlt,
+  FaDownload,
+  FaApple,
+  FaAndroid,
+  FaWindows,
+  FaApplePay,
+  FaInstagram,
+} from "react-icons/fa";
 
 const FooterSection = () => {
-
   function FooterComponent() {
     return (
       <Footer className="bg-gradient-to-r from-yellow-500 via-orange-500 to-red-500 text-white font-bold relative overflow-hidden">
@@ -84,29 +98,33 @@ const FooterSection = () => {
               <Footer.LinkGroup col>
                 <Footer.Link href="#">
                   <div className="flex items-center gap-x-2 text-lg">
-                    <FaApple /> <p>iOS</p>
+                    <FaApple size={22} /> <p>iOS</p>
                   </div>
                 </Footer.Link>
                 <Footer.Link href="#">
                   <div className="flex items-center gap-x-2 text-lg">
-                    <FaAndroid /> <p>Android</p>
+                    <FaAndroid size={22} /> <p>Android</p>
                   </div>
                 </Footer.Link>
                 <Footer.Link href="#">
                   <div className="flex items-center gap-x-2 text-lg">
-                    <FaWindows /> <p>Windows</p>
+                    <FaWindows size={22} /> <p>Windows</p>
                   </div>
                 </Footer.Link>
                 <Footer.Link href="#">
                   <div className="flex items-center gap-x-2 text-lg">
-                    <FaApplePay /> <p>MacOS</p>
+                    <FaApplePay size={33} /> <p>MacOS</p>
                   </div>
                 </Footer.Link>
               </Footer.LinkGroup>
             </div>
           </div>
           <div className="w-full bg-gray-700 px-4 py-6 sm:flex sm:items-center sm:justify-between footer-bottom relative z-10">
-            <Footer.Copyright href="#" by="UiPath" year={new Date().getFullYear()} />
+            <Footer.Copyright
+              href="#"
+              by="UiPath"
+              year={new Date().getFullYear()}
+            />
             <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
               <Footer.Icon href="#" icon={FaFacebook} />
               <Footer.Icon href="#" icon={FaInstagram} />
@@ -119,10 +137,10 @@ const FooterSection = () => {
   }
 
   return (
-    <div className='relative'>
+    <div className="relative">
       <FooterComponent />
     </div>
   );
-}
+};
 
 export default FooterSection;
