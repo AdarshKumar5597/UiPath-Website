773d10630f2fbabe8e04e501f9a4f3b85599bb96
AdarshKumar5597 <adarshkumar5597@gmail.com>
2024-01-13 20:19:56 +0530

    Testimonial Part Done

--- src/components/Home/FacultyReviewCard.js
+++ src/components/Home/FacultyReviewCard.js
@@ -3,9 +3,12 @@ import User from "../../assets/images/user.jpeg"
 
 const FacultyReviewCard = ({name}) => {
   return (
-    <div className='box relative flex items-center justify-center shadow-lg shadow-black'>
-        <div className='wave -one'></div>
-        <img src={User} alt="user" className="absolute w-[90%] h-[80%] translate-y-[-15px] rounded-md shadow-sm shadow-yellow-300"/>
+    <div className='relative flex items-center gap-x-7 bg-white p-2'>
+        <img src={User} alt="user" className="w-[100px] h-[100px] rounded-full"/>
+        <div className='flex flex-col gap-y-1 justify-evenly'>
+          <p className=' font-bold text-xl'>{name}</p>
+          <p className=' text-[#787A80] text-xl'>{name}</p>
+        </div>
     </div>
   )
 }
--- src/sections/Home/FacultyReview.js
+++ src/sections/Home/FacultyReview.js
@@ -7,45 +7,37 @@ import 'react-animated-slider/build/horizontal.css';
 const FacultyReview = () => {
 
     return (
-        <div className="w-[100vw] h-[90vh] flex flex-col justify-center items-center relative gap-y-5 mt-[-80px]">
-            <div className='flex max-w-[1080px] w-[1080px] items-center px-5'>
-                <p className='font-oswald text-5xl text-orange-600 w-[400px]' style={{ backgroundImage: 'linear-gradient(45deg, red, orange, yellow)', WebkitBackgroundClip: 'text', color: 'transparent' }}>
-                    Faculty Reviews
+        <div className="w-[100vw] h-[90vh] flex flex-col gap-y-5 bg-[#F4F5F6] items-center justify-center mt-[0px]">
+            <div className='flex flex-col max-w-[1080px] w-[1080px] items-center justify-center gap-y-3'>
+                <p className='font-oswald text-xl text-black '>
+                    Testimonials
                 </p>
-                <div className="flex relative items-center w-full">
-                    <div className="h-[3px] rounded-full w-[700px] overflow-hidden absolute animate-width">
-                        <div className="h-full bg-gradient-to-r from-orange-500 via-red-500 to-yellow-500"></div>
-                    </div>
-                    <div
-                        className="bg-yellow-500 h-[30px] w-[30px] rounded-full flex items-center justify-center border border-yellow-500 absolute left-[715px] animate-ball">
-                        <div className="bg-orange-500 h-[15px] w-[15px] rounded-full shadow-md"></div>
-                    </div>
-                </div>
+                <p className='font-oswald text-5xl text-black'>
+                    What Others Say
+                </p>
             </div>
-            <Slider autoplay={2000}>
+            <div className='w-full flex justify-center items-center'>
+                <Slider autoplay={1000}>
                 {FacultyData.map((faculty, index) => (
                     <div
                         key={index}
-                        className='flex flex-row items-center justify-center p-2 gap-x-2'
-                        style={{background:"no-repeat center center", backgroundColor:"#ffa500", width:"1080px"}}
+                        className='flex flex-col items-center justify-center px-32 bg-[#F4F5F6]'
+                        style={{ background: "no-repeat center center", backgroundColor: "#fff", width: "1080px" }}
                     >
-                        <div className="transition-transform transform hover:scale-[102%]">
-                            <FacultyReviewCard name={faculty.name} />
-                        </div>
                         <div
-                            className="bg-white rounded-md w-[500px] flex flex-col items-center justify-evenly h-[225px] shadow-md shadow-black font-bold font-oswald p-5"
-                            style={{ background: 'linear-gradient(to right, white, yellow)' }}
+                            className="bg-white w-[full] h-[full] flex flex-col justify-evenly p-5 gap-y-3"
                         >
-                            <p>
-                                <span className="text-superscript text-3xl">'' </span>
+                            <p className=' text-2xl'>
                                 {faculty.review}
                             </p>
-                            <span className="text-superscript text-3xl text-end w-[90%]">''</span>
-                            <p className="text-end w-[100%]">By - {faculty.name}</p>
+                            <div>
+                                <FacultyReviewCard name={faculty.name} />
+                            </div>
                         </div>
                     </div>
                 ))}
             </Slider>
+            </div>
 
         </div>
     );
