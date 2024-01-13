02d460030ee6e87d275823599af072d726afdbe0
hritesh04 <hritesh04.dev@gmail.com>
2024-01-13 23:53:03 +0530

    latestPosts and footer added

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import Intro from '../sections/Home/Intro'
-import Motto from '../sections/Home/Motto'
-import Leads from '../sections/Home/Leads'
-import FacultyReview from '../sections/Home/FacultyReview'
+import React from "react";
+import Intro from "../sections/Home/Intro";
+import Motto from "../sections/Home/Motto";
+import Leads from "../sections/Home/Leads";
+import FacultyReview from "../sections/Home/FacultyReview";
+import LatestPosts from "../sections/Home/LatestPosts";
 
 const Home = () => {
   return (
     <div>
-        <Intro/>
-        <Motto/>
-        <Leads/>
-        <FacultyReview/>
+      <Intro />
+      <Motto />
+      <Leads />
+      <FacultyReview />
+      <LatestPosts />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
