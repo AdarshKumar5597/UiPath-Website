64306f49046db772c416842ebfec0a3501db1e36
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2024-01-15 00:05:50 +0530

    feat : added a temporary data 'data.js' file to store temporary data

---
+++ src/data.js
@@ -0,0 +1,17 @@
+const upcomingEvents = [
+    {
+        eventId:1,
+        event:'Campus Convergence 2024:engaging evening of networking and camaraderie as we come together offline to share experiences, insights, and laughter.',
+        date:{
+            day:'20',
+            month:'January',
+            year:'2024'
+        },
+        time:{
+            start:'8:00 PM',
+            end:'10:00 PM'
+        }
+    }
+]
+
+export{upcomingEvents}
