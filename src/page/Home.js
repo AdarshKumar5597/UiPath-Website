e34c7eb9adc8468c52cb86269c5603bf36bae2cd
AdarshKumar5597 <113282271+AdarshKumar5597@users.noreply.github.com>
2024-01-14 16:00:29 +0530

    Update Home.js

--- src/page/Home.js
+++ src/page/Home.js
@@ -4,7 +4,6 @@ import Motto from "../sections/Home/Motto";
 import Leads from "../sections/Home/Leads";
 import FacultyReview from "../sections/Home/FacultyReview";
 import LatestPosts from "../sections/Home/LatestPosts";
-import React from 'react'
 import Intro from '../sections/Home/Intro'
 import Motto from '../sections/Home/Motto'
 import Leads from '../sections/Home/Leads'
