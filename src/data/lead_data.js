bfc02d3f5d118e27f47831016e48ad653da2c484
Kaushik Verma <kaushikverma321@gmail.com>
2024-01-15 22:45:47 +0530

    Lead Image

--- src/data/lead_data.js
+++ src/data/lead_data.js
@@ -1,95 +1,175 @@
 const LeadData = [
     {
         index:0,
-        src:"../assets/images/user.jpeg",
-        name:"Random",
-        postion:"Position",
-        instagram:"URL",
-        facebook:"URL",
-        linkedin:"URL"
+        src:require("../assets/images/leads/atri.jpg"),
+        name:"Atri Bandyopadhyay",
+        postion:"Coordinator & UiPath Automation Tools",
+        instagram:"https://www.instagram.com/atriclicky?igsh=enQwdG5xNmNqdDNv",
+        gamil:"atricc03@gmail.com",
+        linkedin:"https://www.linkedin.com/in/atri-/"
     },
     {
         index:1,
-        src:"../assets/images/user.jpeg",
-        name:"Random",
-        postion:"Position",
-        instagram:"URL",
-        facebook:"URL",
-        linkedin:"URL"
+        src:require("../assets/images/user.jpg"),
+        name:"Chaman Kumar",
+        postion:"Graphic",
+        instagram:"https://www.instagram.com/87nehal/",
+        gamil:"87nehal@gmail.com",
+        linkedin:"https://www.linkedin.com/in/87nehal/"
     },
     {
         index:2,
-        src:"../assets/images/user.jpeg",
-        name:"Random",
-        postion:"Position",
-        instagram:"URL",
-        facebook:"URL",
-        linkedin:"URL"
+        src:require("../assets/images/leads/deep.jpg"),
+        name:"Deepsikha Das",
+        postion:"Social Media Marketing & Associate Coordinator",
+        instagram:"https://www.instagram.com/deepsikhadas._?igsh=MWRsd2dmOXBzeG12ZQ==",
+        gamil:"deepsikha1104@gmail.com",
+        linkedin:"https://www.linkedin.com/in/devangi-das-667935209/"
     },
     {
         index:3,
-        src:"../assets/images/user.jpeg",
-        name:"Random",
-        postion:"Position",
-        instagram:"URL",
-        facebook:"URL",
-        linkedin:"URL"
+        src:require("../assets/images/leads/saptak.jpg"),
+        name:"Saptak Das",
+        postion:"Vice-Coordinator & Phtography",
+        instagram:"https://www.instagram.com/s.saptak6?igsh=aWNuMHRyY2F4Z2Ni",
+        gamil:"saptakdas84@gmail.com",
+        linkedin:"https://www.linkedin.com/in/saptak-das-as1r2i3/"
     },
     {
         index:4,
-        src:"../assets/images/user.jpeg",
-        name:"Random",
-        postion:"Position",
-        instagram:"URL",
-        facebook:"URL",
-        linkedin:"URL"
+        src:require("../assets/images/leads/muktesh.jpg"),
+        name:"Muktesh Mishra",
+        postion:"Event Management",
+        instagram:"https://www.instagram.com/som._.muktesh?igsh=MTQ4MzkzbmgwYm5yNA==",
+        gamil:"mukteshm24@gmail.com",
+        linkedin:"https://www.linkedin.com/in/muktesh-m/"
     },
     {
         index:5,
-        src:"../assets/images/user.jpeg",
-        name:"Random",
-        postion:"Position",
-        instagram:"URL",
-        facebook:"URL",
-        linkedin:"URL"
+        src:require("../assets/images/leads/Anuska.jpg"),
+        name:"Anuska Maity",
+        postion:"Associate Coordinator & Content Writing",
+        instagram:"https://www.instagram.com/anuska.maity?igsh=cDRuZnc2bjZ5b21t",
+        gamil:"anuska09.maity@gmail.com",
+        linkedin:"https://www.linkedin.com/in/anuska-maity-706340253/"
     },
     {
         index:6,
-        src:"../assets/images/user.jpeg",
-        name:"Random",
-        postion:"Position",
-        instagram:"URL",
-        facebook:"URL",
-        linkedin:"URL"
+        src:require("../assets/images/leads/Devangi.jpg"),
+        name:"Devangi Das",
+        postion:"Web Development",
+        instagram:"https://www.instagram.com/exquisite_artemis?igsh=MWtudXlqbmltYjcwOQ==",
+        gamil:"devangiofficial@gmail.com",
+        linkedin:"https://www.linkedin.com/in/devangi-das-667935209/"
     },
     {
         index:7,
-        src:"../assets/images/user.jpeg",
-        name:"Random",
-        postion:"Position",
-        instagram:"URL",
-        facebook:"URL",
-        linkedin:"URL"
+        src:require("../assets/images/leads/disha.jpg"),
+        name:"M Disha",
+        postion:"Associate Coordinator & RPA & AI",
+        instagram:"https://www.instagram.com/thesecret_disha?igsh=MXMxOGQ1dGRoNzZqbg==",
+        gamil:"manvidisha0803@gmail.com",
+        linkedin:"https://www.linkedin.com/in/m-disha-5a1606213/"
     },
     {
         index:8,
-        src:"../assets/images/user.jpeg",
-        name:"Random",
-        postion:"Position",
-        instagram:"URL",
-        facebook:"URL",
-        linkedin:"URL"
+        src:require("../assets/images/leads/Mousumi.jpg"),
+        name:"Mousumi Dey",
+        postion:"Social Media Marketing",
+        instagram:"https://www.instagram.com/mousumi.dey10?igsh=eWljZDh4cDR6OTFz",
+        gamil:"deymousumi1221@gmail.com",
+        linkedin:"https://www.linkedin.com/in/mousumi-dey-8961b3201/"
     },
     {
         index:9,
-        src:"../assets/images/user.jpeg",
-        name:"Random",
-        postion:"Position",
-        instagram:"URL",
-        facebook:"URL",
+        src:require("../assets/images/leads/saswat.jpg"),
+        name:"Saswat Jena",
+        postion:"Video Editing",
+        instagram:"https://www.instagram.com/sj_k_i_n_g?igsh=MXBtcG5ubXB0eGd2eg==",
+        gamil:"saswat.jena.2003sj@gmail.com",
+        linkedin:"https://www.linkedin.com/in/saswat-jena-32098b1b8?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+    },
+    {
+        index:10,
+        src:require("../assets/images/leads/avirup.JPG"),
+        name:"Avirup Banerjee",
+        postion:"Event Management",
+        instagram:"https://www.instagram.com/avirupbanerjee_02/?igsh=Z2t4OHN1NDZiZWsz",
+        gamil:"bavirup2004@gmail.com",
+        linkedin:"https://www.linkedin.com/in/avirup-banerjee-949973243/"
+    },
+    {
+        index:11,
+        src:require("../assets/images/leads/Komal.jpg"),
+        name:"Komal Jaiswal",
+        postion:"Social Media Marketing",
+        instagram:"https://www.instagram.com/komaljaiswal237?igsh=MTlpNjdwMHk2bHpheg==",
+        gamil:"URL",
+        linkedin:"https://www.linkedin.com/in/komal-jaiswal-9347522a6/"
+    },
+    {
+        index:12,
+        src:require("../assets/images/leads/mayank.jpg"),
+        name:"Mayank Singhal",
+        postion:"Machine Learning",
+        instagram:"https://www.instagram.com/singhalmynk?igsh=ZzJrYWx1YTkzN3Vj",
+        gamil:"mayanksinghal831@gmail.com",
+        linkedin:"https://www.linkedin.com/in/singhalmynk/"
+    },
+    {
+        index:13,
+        src:require("../assets/images/leads/yatish.jpg"),
+        name:"Yatish Raj",
+        postion:"Graphic Designing ",
+        instagram:"https://www.instagram.com/akayatish?igsh=cWRhMHdmaHB3ZWZp",
+        gamil:"atulagrx@gmail.com",
+        linkedin:"https://www.linkedin.com/in/atul1901/"
+    },
+    {
+        index:14,
+        src:require("../assets/images/leads/sushant.jpg"),
+        name:"Sushant",
+        postion:"Video Editing",
+        instagram:"https://www.instagram.com/sj_k_i_n_g?igsh=MTdxbmp6M2xoMHdkcg==",
+        gamil:"sushantchoudhary586@gmail.com",
         linkedin:"URL"
     },
-
+    {
+        index:15,
+        src:require("../assets/images/leads/akash.jpg"),
+        name:"Akash Sadhab",
+        postion:"Web development",
+        instagram:"https://www.instagram.com/mr_sadhab?igsh=bmU3NXkwM2V0dGFh",
+        gamil:"akashkumarsadhab@gmail.com",
+        linkedin:"https://www.linkedin.com/in/akash-sadhab/"
+    },
+    {
+        index:16,
+        src:require("../assets/images/user.jpg"),
+        name:"Prasun Chakraborty",
+        postion:"Vice Coordinator & UiPath Automation Tools",
+        instagram:"https://www.instagram.com/uipathglobal/",
+        gamil:"URL",
+        linkedin:"https://www.linkedin.com/company/uipath/"
+    },
+    {
+        index:17,
+        src:require("../assets/images/leads/Arojeet.jpg"),
+        name:"Arojeet Talukdar",
+        postion:"Event Management",
+        instagram:"https://www.instagram.com/avirupbanerjee_02?igsh=Z2t4OHN1NDZiZWsz",
+        gamil:"arojeettaluckdar@gmail.com",
+        linkedin:"https://www.linkedin.com/in/arojeet-talukdar-2605001b2/"
+    },
+    {
+        index:18,
+        src:require("../assets/images/leads/sourabh.png"),
+        name:"Sourabh Mohanty",
+        postion:"RPA & AI",
+        instagram:"https://www.instagram.com/sourabhrex?igsh=cXI5ZTZ4c2hmdjly",
+        gamil:"mohantysourabh007@gmail.com",
+        linkedin:"https://www.linkedin.com/in/sourabh-mohanty-44110a208/"
+    },
 ]
 
 export default LeadData;
--- src/sections/Home/Leads.js
+++ src/sections/Home/Leads.js
@@ -1,7 +1,8 @@
 import React from "react";
 import LeadData from "../../data/lead_data";
-import UserPic from "../../assets/images/user.jpeg";
-import { ReactComponent as Facebook } from "../../assets/icon/facebook-fill.svg";
+// import UserPic from "../../assets/images/user.jpeg";
+// import deep from "../../assets/images/leads/deep.jpg"
+import { ReactComponent as Gmail } from "../../assets/icon/mail-fill.svg";
 import { ReactComponent as Instagram } from "../../assets/icon/instagram-fill.svg";
 import { ReactComponent as Linkedin } from "../../assets/icon/linkedin-fill.svg";
 const Leads = () => {
@@ -20,25 +21,25 @@ const Leads = () => {
 
                         <div className="h-[367.84px] w-[308.33px] group relative items-center justify-center">
 
-                            <img className="h-full w-full object-cover" src={UserPic} alt="lead" />
+                            <img className="h-full w-full object-cover" src={lead.src} alt="lead" />
 
                             <div class="flex-row items-center justify-end gap-2 opacity-0 group-hover:opacity-100 transition-all duration-500">
 
                                 <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black/50"></div>
 
                                 <div className="flex flex-row items-center justify-end gap-2 translate-y-[100%] group-hover:translate-y-0 transition-all duration-500 relative bottom-14 right-4">
-                                    <a href={lead.facebook}>
+                                    <a href={lead.facebook} target="blank">
                                         <div className="rounded-full bg-gray-400 w-[34.62px] h-[34.62px] flex justify-center items-center">
-                                            <Facebook className="hover:fill-[#316FF6] w-[17.31px] h-[17.31px]" />
+                                            <Gmail className="hover:fill-[#fafafa] w-[17.31px] h-[17.31px]" />
                                         </div>
                                     </a>
-                                    <a href={lead.instagram}>
+                                    <a href={lead.instagram} target="blank">
                                         <div className="rounded-full bg-gray-400 w-[34.62px] h-[34.62px] flex justify-center items-center">
                                             <Instagram className="hover:fill-[#F52F6E] w-[17.31px] h-[17.31px]" />
                                         </div>
                                     </a>
 
-                                    <a href={lead.linkedin}>
+                                    <a href={lead.linkedin} target="blank">
                                         <div className="rounded-full bg-gray-400 w-[34.62px] h-[34.62px] flex justify-center items-center">
                                             <Linkedin className="hover:fill-[#0077b5] w-[17.31px] h-[17.31px]" />
                                         </div>
