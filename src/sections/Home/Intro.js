cca746c43ccefa58d8f4b2e1c83a92ae6013ae57
Yash Raj Singh <126608859+yashraj1706@users.noreply.github.com>
2024-01-16 18:05:13 +0530

    added section 1 with synced repo

--- src/sections/Home/Intro.js
+++ src/sections/Home/Intro.js
@@ -1,45 +1,33 @@
-import React, { useEffect, useRef } from 'react'
-import BuildingLogo from "../../assets/images/building.png"
-import Typed from 'typed.js';
+import React from "react";
+import { useRef } from "react";
+import Navbar from "../../layout/main/Navbar.js";
+import Auto from "../../assets/images/auto_gif_3.gif";
+import Orangedot from "../../assets/images/Orangecirc.png";
 
-const Intro = () => {
-    const el = useRef(null);
+function Intro(){
 
-    useEffect(() => {
-        const typed = new Typed(el.current, {
-            strings: ["Grow", "Develop", "Learn"], // Strings to display
-            // Speed settings, try diffrent values untill you get good results
-            startDelay: 300,
-            typeSpeed: 250,
-            backSpeed: 250,
-            backDelay: 100,
-            loop: true,
-            showCursor: true,
-            cursorChar: "!"
-        });
-
-        // Destropying
-        return () => {
-            typed.destroy();
-        };
-    }, []);
-    return (
-        <div className="w-[100vw] h-[90vh] flex justify-center items-center">
-            <div className="max-w-[1080px] flex justify-center items-center px-5">
-                <div className="w-[1080px] h-[400px] bg-[#ffa500] rounded-lg flex flex-col items-start relative gap-y-5 pl-20 justify-center">
-                    <p className="text-9xl font-bold neumorphic-text">
-                        UiPath
-                    </p>
-                    <p className="text-5xl font-bold font-oswald">
-                        A Community <br /> Where We <span className="text-7xl neumorphic-text" ref={el}></span>
-                    </p>
-                    <div className="absolute right-[-160px] top-[-180px]">
-                        <img src={BuildingLogo} className="h-[800px] w-[700px]"></img>
-                    </div>
+    return(
+        <div className="bgc w-full h-screen">
+            <Navbar />
+            <div className="flex w-full h-5/6 justify-end items-center">
+                <div class="flex w-4/12 h-full justify-around items-center">
+                    <span className="font-bold text-black w-1/2 h-full flex flex-col justify-center items-center"><span className="text-9xl py-0 bg-gradient-to-br from-orange-700  via-orange-300 to-orange-700 grad bg-clip-text text-transparent">UiPath</span><span className="text-2xl py-0 text-center w-[400px]">Student Community KIIT</span><span></span></span>
+                    {/* <span className="text-7xl neumorphic-text" ref={el}></span> */}
+                </div>
+                <div className="flex w-8/12 h-full  justify-around items-center">
+                    <img src={Auto} alt="" className="h-96 rounded-3xl" />
                 </div>
             </div>
+
+            <div id="accomplishments" className="w-full h-32">
+                <span className="flex justify-center items-center gap-10">
+                    <span className="font-semibold text-2xl flex items-end gap-3 "> <span className="font-semibold text-6xl bottom-textS1 bg-clip-text text-transparent">12</span><span>Domains</span></span><img src={Orangedot} alt="" className="w-4" />
+                    <span className="font-semibold text-2xl flex items-end gap-3 "> <span className="font-semibold text-6xl bottom-textS1 bg-clip-text text-transparent">1</span><span>Event Organized</span></span><img src={Orangedot} alt="" className="w-4" />
+                    <span className="font-semibold text-2xl flex items-end gap-3 "> <span className="font-semibold text-6xl bottom-textS1 bg-clip-text text-transparent">1</span><span>Challenge Completed</span></span>
+                    </span>
+            </div>
         </div>
-    )
+    );
 }
 
-export default Intro
+export default Intro;
