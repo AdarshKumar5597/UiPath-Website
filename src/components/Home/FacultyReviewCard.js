0d9892e027d8273b8420f9d31f9a033a1ead36b1
AdarshKumar5597 <adarshkumar5597@gmail.com>
2024-02-05 01:31:36 +0530

    Responsive Faculty Review

--- src/components/Home/FacultyReviewCard.js
+++ src/components/Home/FacultyReviewCard.js
@@ -1,15 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-const FacultyReviewCard = ({name, src}) => {
+const FacultyReviewCard = ({ name, src }) => {
   return (
-    <div className='relative flex items-center gap-x-7 bg-white p-2'>
-        <img src={src} alt="user" className="w-[100px] h-[100px] rounded-full"/>
-        <div className='flex flex-col gap-y-1 justify-evenly'>
-          <p className=' font-bold text-xl'>{name}</p>
-          <p className=' text-[#787A80] text-xl'>{name}</p>
-        </div>
+    <div className='relative flex items-center gap-x-4 bg-white p-2 frc'>
+      <img src={src} alt="user" className="w-[80px] h-[80px] sm:w-[50px] sm:h-[50px] facultyImage rounded-full" />
+      <div className='flex flex-col gap-y-1 justify-evenly'>
+        <p className='font-bold text-lg md:text-base facultyText'>{name}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FacultyReviewCard
+export default FacultyReviewCard;
--- src/sections/Home/FacultyReview.js
+++ src/sections/Home/FacultyReview.js
@@ -1,46 +1,40 @@
-import React from 'react';
-import FacultyReviewCard from '../../components/Home/FacultyReviewCard';
-import FacultyData from '../../data/faculty_data';
-import Slider from 'react-animated-slider';
-import 'react-animated-slider/build/horizontal.css';
+import React from "react";
+import FacultyReviewCard from "../../components/Home/FacultyReviewCard";
+import FacultyData from "../../data/faculty_data";
+import Slider from "react-animated-slider";
+import "react-animated-slider/build/horizontal.css";
 
 const FacultyReview = () => {
-
-    return (
-        <div className="w-[100vw] h-[90vh] flex flex-col gap-y-5 bg-[#F4F5F6] items-center justify-center mt-[0px]">
-            <div className='flex flex-col max-w-[1080px] w-[1080px] items-center justify-center gap-y-3'>
-                <p className='font-oswald text-xl text-black '>
-                    Testimonials
+  return (
+    <div className="w-[100vw] h-[90vh] flex flex-col gap-y-5 bg-[#F4F5F6] items-center justify-center mt-[0px]">
+      <div className="flex flex-col max-w-[1080px] w-[1080px] items-center justify-center gap-y-3">
+        <p className="font-oswald text-xl text-black ">Testimonials</p>
+        <p className="font-oswald text-5xl text-black">What Others Say</p>
+      </div>
+      <div className="w-full flex justify-center items-center">
+        <Slider autoplay={1000}>
+          {FacultyData.map((faculty, index) => (
+            <div
+              key={index}
+              className="flex flex-col items-center justify-center px-32 sliderCard bg-[#F4F5F6] py-5 lg:max-w-[1080px] md:max-w-[1080px]"
+              // style={{ background: "no-repeat center center", backgroundColor: "#fff" }}
+            >
+              <div className="bg-white flex-wrap flex flex-col justify-evenly p-5 gap-y-3 reviewBox h-[400px] w-[1080px]">
+                <p className="lg:text-xl md:text-lg sm:text-sm reviewText">
+                  {faculty.review.length >= 630
+                    ? faculty.review.slice(0, 630) + "..."
+                    : faculty.review + " ".repeat(630 - faculty.review.length)}
                 </p>
-                <p className='font-oswald text-5xl text-black'>
-                    What Others Say
-                </p>
+                <div>
+                  <FacultyReviewCard name={faculty.name} src={faculty.src} />
+                </div>
+              </div>
             </div>
-            <div className='w-full flex justify-center items-center'>
-                <Slider autoplay={1000}>
-                {FacultyData.map((faculty, index) => (
-                    <div
-                        key={index}
-                        className='flex flex-col items-center justify-center px-32 bg-[#F4F5F6] py-5'
-                        style={{ background: "no-repeat center center", backgroundColor: "#fff", width: "1080px" }}
-                    >
-                        <div
-                            className="bg-white w-[full] h-[full] flex flex-col justify-evenly p-5 gap-y-3"
-                        >
-                            <p className=' text-xl'>
-                                {faculty.review}
-                            </p>
-                            <div>
-                                <FacultyReviewCard name={faculty.name} src={faculty.src} />
-                            </div>
-                        </div>
-                    </div>
-                ))}
-            </Slider>
-            </div>
-
-        </div>
-    );
+          ))}
+        </Slider>
+      </div>
+    </div>
+  );
 };
 
 export default FacultyReview;
