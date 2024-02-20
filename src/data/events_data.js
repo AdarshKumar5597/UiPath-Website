95b47d3b82db5ad013d943b50225900614f10e90
hritesh04 <hritesh04.dev@gmail.com>
2024-02-20 23:21:27 +0530

    scrolling fix

---
+++ src/data/events_data.js
@@ -0,0 +1,10 @@
+export const eventsData = [
+  {
+    date: "20 JAN",
+    name: "ORIENTATION SYNERGY",
+    descripton:
+      "Anticipation is in the air! Brace yourselves for an evening filled with excitement and delight surprises at our officail meet on Januaray 20th. Get ready to be amazed as we unfold unexpected moments and celebrate the extraordinary together!",
+    venue: "CAMPUS 14, SEMINAR HALL",
+    link: "https://www.instagram.com/p/C2RWUf4hOEZ/?igsh=czE0MmpuaG5wb29z",
+  },
+];
