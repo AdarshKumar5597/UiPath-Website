40544008dd2cb80ab9f2699e0aca41def1cdecd3
hritesh04 <hritesh04.dev@gmail.com>
2024-02-21 12:52:10 +0530

    added gallery and admin gallery

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ import { AdminLayout } from "../layout/adminDashboard/DashboardLayout";
 import { AdminMembers } from "../components/adminDashboard/AdminMembers";
 import { MemberEdit } from "../components/adminDashboard/MemberEdit";
 import { AdminProjects } from "../components/adminDashboard/AdminProjects";
-import { AdminEvents } from "../components/adminDashboard/Events";
+import { AdminEvents } from "../components/adminDashboard/AdminEvent";
 import { AdminDashboard } from "../components/adminDashboard/Dashboard";
 import { AdminPosts } from "../components/adminDashboard/AdminPosts";
 import { PostEdit } from "../components/adminDashboard/PostEdit";
@@ -14,6 +14,9 @@ import { ProjectEdit } from "../components/adminDashboard/ProjectEdit";
 import ProjectPage from "../page/project";
 import { Events } from "../page/Events";
 import GalleryOne from "../page/GalleryOne";
+import { EventsEdit } from "../components/adminDashboard/EventsEdit";
+import { AdminGallery } from "../components/adminDashboard/AdminGallery";
+import { GalleryEdit } from "../components/adminDashboard/GalleryEdit";
 export default function Router() {
   return useRoutes([
     {
@@ -38,10 +41,13 @@ export default function Router() {
         { path: "members", element: <AdminMembers /> },
         { path: "members/:id", element: <MemberEdit /> },
         { path: "projects", element: <AdminProjects /> },
+        { path: "projects/add", element: <ProjectEdit /> },
         { path: "events", element: <AdminEvents /> },
+        { path: "events/:id", element: <EventsEdit /> },
         { path: "posts", element: <AdminPosts /> },
         { path: "posts/:id", element: <PostEdit /> },
-        { path: "projects/add", element: <ProjectEdit /> },
+        { path: "gallery", element: <AdminGallery /> },
+        { path: "gallery/:id", element: <GalleryEdit /> },
       ],
     },
   ]);
