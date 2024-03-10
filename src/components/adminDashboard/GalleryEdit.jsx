5c63646473c3ed3410d4d66292b4e40843ac529f
Hritesh Mondal <134622021+hritesh04@users.noreply.github.com>
2024-03-11 01:22:06 +0530

    updates (#36)
    * updates

--- src/components/adminDashboard/GalleryEdit.jsx
+++ src/components/adminDashboard/GalleryEdit.jsx
@@ -1,23 +1,58 @@
 import { CiCamera } from "react-icons/ci";
 import { useParams } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 import recuitment from "./../../assets/images/latestPosts/WhatsApp Image 2024-01-15 at 00.32.31_e6178c13.jpg";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import handleImageUpload from "../../utils/uploadImages";
 export const GalleryEdit = (post) => {
+  const [images, setImages] = useState([]);
+  const [title, setTitle] = useState("");
   const { id } = useParams();
+  const navigate = useNavigate();
   const galleryImg = useRef();
+  console.log(images);
+  const handleSubmit = async () => {
+    console.log(title);
+    console.log(images);
+    //const response = await handleImageUpload(images, "gallery");
+    //console.log(response);
+    const response = await fetch(
+      process.env.REACT_APP_BACKEND_BASE_URL + "gallery",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: title,
+          photoUrls: images,
+        }),
+      }
+    );
+    if (response.ok) {
+      navigate("/admin/gallery");
+    }
+  };
   return (
     <div className="h-full w-full p-2 flex gap-2  overflow-scroll overflow-x-hidden bg-white flex-col">
       <div className="w-full h-full">
         <div className="w-full p-4 flex h-full rounded-xl">
-          <div className="h-full w-1/2">
-            <h1 className=" font-bold mb-2">Title</h1>
-            <input
-              type="email"
-              placeholder={post?.title || "Email"}
-              className="border-2 w-full p-2 rounded-xl"
-            />
+          <div className="h-full flex flex-col justify-between w-1/2">
+            <div>
+              <h1 className=" font-bold mb-2">Title</h1>
+              <input
+                type="email"
+                placeholder={title || "Gallery Title"}
+                onChange={(e) => setTitle(e.target.value)}
+                className="border-2 w-full p-2 rounded-xl"
+              />
+            </div>
+            <button
+              className=" bg-blue-700 rounded-xl text-white h-fit w-full text-center p-4"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
           </div>
           <div className="h-full w-1/2 p-4">
             <div className="  rounded-xl h-full w-full">
@@ -44,7 +79,10 @@ export const GalleryEdit = (post) => {
                       />
                       <input
                         type="file"
-                        onChange={(e) => handleImageUpload(e, "gallery")}
+                        onChange={async (e) => {
+                          const res = await handleImageUpload(e.target.files);
+                          setImages(res.data);
+                        }}
                         className=" hidden"
                         ref={galleryImg}
                         multiple
--- src/utils/uploadImages.js
+++ src/utils/uploadImages.js
@@ -1,12 +1,18 @@
-export default async function handleImageUpload(event, type) {
-  const images = Array.from(event.target.files);
+export default async function handleImageUpload(image) {
+  const images = Array.from(image);
   const formData = new FormData();
   for (const img of images) {
     formData.append("image", img);
   }
   console.log(process.env.REACT_APP_BACKEND_BASE_URL);
-  const response = await fetch(process.env.REACT_APP_BACKEND_BASE_URL + type, {
-    method: "POST",
-    body: formData,
+  const response = await fetch(
+    process.env.REACT_APP_BACKEND_BASE_URL + "upload",
+    {
+      method: "POST",
+      body: formData,
+    }
+  ).then((data) => {
+    return data.json();
   });
+  return response;
 }
