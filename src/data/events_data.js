8c3a14e008f899f3db566347eaa54043e65c72fa
Raima Mukherjee <92201755+raimamukherjee29@users.noreply.github.com>
2024-03-11 12:19:06 +0530

    changed blogs, added cards (#38)
    * changed blogs
    
    * upcoming events

--- src/data/events_data.js
+++ src/data/events_data.js
@@ -1,10 +1,26 @@
 export const eventsData = [
   {
-    date: "20 JAN",
-    name: "ORIENTATION SYNERGY",
+    date: '20 JAN',
+    name: 'ORIENTATION SYNERGY',
     descripton:
-      "Anticipation is in the air! Brace yourselves for an evening filled with excitement and delight surprises at our officail meet on Januaray 20th. Get ready to be amazed as we unfold unexpected moments and celebrate the extraordinary together!",
-    venue: "CAMPUS 14, SEMINAR HALL",
-    link: "https://www.instagram.com/p/C2RWUf4hOEZ/?igsh=czE0MmpuaG5wb29z",
+      'Anticipation is in the air! Brace yourselves for an evening filled with excitement and delight surprises at our officail meet on Januaray 20th. Get ready to be amazed as we unfold unexpected moments and celebrate the extraordinary together!',
+    venue: 'CAMPUS 14, SEMINAR HALL',
+    link: 'https://www.instagram.com/p/C2RWUf4hOEZ/?igsh=czE0MmpuaG5wb29z',
   },
-];
+  {
+    date: '28 JAN',
+    name: 'RECRUITMENT OF 1ST YEAR',
+    descripton:
+      "Calling all first-years! Join our dynamic society and experience a year filled with fun events and leadership roles. Grow as a person and professionally, while building your college network. It's the perfect way to shape your college experience!",
+    venue: 'CAMPUS 14, SEMINAR HALL',
+    link: 'https://www.instagram.com/p/C2RWUf4hOEZ/?igsh=czE0MmpuaG5wb29z',
+  },
+  {
+    date: '2 MARCH',
+    name: 'USC KIIT RESEARCH FORUM',
+    descripton:
+      'Get ready to embark on an exciting journey with the inauguration of the USC KIIT Research Forum! Join us as we dive into collaborative exploration, where your ideas ignite and innovations flourish under the guidance of industry-experienced mentors.',
+    venue: 'USC KIIT',
+    link: 'https://www.instagram.com/p/C2RWUf4hOEZ/?igsh=czE0MmpuaG5wb29z',
+  },
+]
