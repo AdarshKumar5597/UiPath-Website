7719698904afa4bac2d911659825ed39b36d011c
raimamukherjee29 <raimamukherjee2910@gmail.com>
2024-03-11 11:38:00 +0530

    changed blogs

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,56 +1,58 @@
-import { useRoutes } from "react-router-dom";
-import MainLayout from "../layout/main/MainLayout";
-import { SignInPage } from "../layout/signin/SignInPage";
-import { AdminLayout } from "../layout/adminDashboard/DashboardLayout";
-import { AdminMembers } from "../components/adminDashboard/AdminMembers";
-import { MemberEdit } from "../components/adminDashboard/MemberEdit";
-import { AdminProjects } from "../components/adminDashboard/AdminProjects";
-import { AdminEvents } from "../components/adminDashboard/AdminEvent";
-import { AdminDashboard } from "../components/adminDashboard/Dashboard";
-import { AdminPosts } from "../components/adminDashboard/AdminPosts";
-import { PostEdit } from "../components/adminDashboard/PostEdit";
-import { DomainLayout } from "../layout/domain/DomainLayout";
-import { ProjectEdit } from "../components/adminDashboard/ProjectEdit";
-import ProjectPage from "../page/project";
-import { Events } from "../page/Events";
-import GalleryOne from "../page/GalleryOne";
-import { EventsEdit } from "../components/adminDashboard/EventsEdit";
-import { AdminGallery } from "../components/adminDashboard/AdminGallery";
-import { GalleryEdit } from "../components/adminDashboard/GalleryEdit";
-import Members from "../components/Members/Members";
+import { useRoutes } from 'react-router-dom'
+import MainLayout from '../layout/main/MainLayout'
+import { SignInPage } from '../layout/signin/SignInPage'
+import { AdminLayout } from '../layout/adminDashboard/DashboardLayout'
+import { AdminMembers } from '../components/adminDashboard/AdminMembers'
+import { MemberEdit } from '../components/adminDashboard/MemberEdit'
+import { AdminProjects } from '../components/adminDashboard/AdminProjects'
+import { AdminEvents } from '../components/adminDashboard/AdminEvent'
+import { AdminDashboard } from '../components/adminDashboard/Dashboard'
+import { AdminPosts } from '../components/adminDashboard/AdminPosts'
+import { PostEdit } from '../components/adminDashboard/PostEdit'
+import { DomainLayout } from '../layout/domain/DomainLayout'
+import { ProjectEdit } from '../components/adminDashboard/ProjectEdit'
+import ProjectPage from '../page/project'
+import { Events } from '../page/Events'
+import GalleryOne from '../page/GalleryOne'
+import { EventsEdit } from '../components/adminDashboard/EventsEdit'
+import { AdminGallery } from '../components/adminDashboard/AdminGallery'
+import { GalleryEdit } from '../components/adminDashboard/GalleryEdit'
+import Members from '../components/Members/Members'
+import Blogs from '../components/Blogs/Blog'
 export default function Router() {
   return useRoutes([
     {
-      path: "/",
+      path: '/',
       element: <MainLayout />,
-      children: [{ path: "*", element: <MainLayout /> }],
+      children: [{ path: '*', element: <MainLayout /> }],
     },
-    { path: "/domains", element: <DomainLayout /> },
-    { path: "/events", element: <Events /> },
-    { path: "/members", element: <Members /> },
-    { path: "/gallery", element: <ProjectPage /> },
-    { path: "/gallery/:id", element: <GalleryOne /> },
+    { path: '/domains', element: <DomainLayout /> },
+    { path: '/events', element: <Events /> },
+    { path: '/members', element: <Members /> },
+    { path: '/Blog', element: <Blogs /> },
+    { path: '/gallery', element: <ProjectPage /> },
+    { path: '/gallery/:id', element: <GalleryOne /> },
     {
-      path: "/signin",
+      path: '/signin',
       element: <SignInPage />,
-      children: [{ path: "*", element: <SignInPage /> }],
+      children: [{ path: '*', element: <SignInPage /> }],
     },
     {
-      path: "/admin",
+      path: '/admin',
       element: <AdminLayout />,
       children: [
-        { path: "dashboard", element: <AdminDashboard /> },
-        { path: "members", element: <AdminMembers /> },
-        { path: "members/:id", element: <MemberEdit /> },
-        { path: "projects", element: <AdminProjects /> },
-        { path: "projects/add", element: <ProjectEdit /> },
-        { path: "events", element: <AdminEvents /> },
-        { path: "events/:id", element: <EventsEdit /> },
-        { path: "posts", element: <AdminPosts /> },
-        { path: "posts/:id", element: <PostEdit /> },
-        { path: "gallery", element: <AdminGallery /> },
-        { path: "gallery/:id", element: <GalleryEdit /> },
+        { path: 'dashboard', element: <AdminDashboard /> },
+        { path: 'members', element: <AdminMembers /> },
+        { path: 'members/:id', element: <MemberEdit /> },
+        { path: 'projects', element: <AdminProjects /> },
+        { path: 'projects/add', element: <ProjectEdit /> },
+        { path: 'events', element: <AdminEvents /> },
+        { path: 'events/:id', element: <EventsEdit /> },
+        { path: 'posts', element: <AdminPosts /> },
+        { path: 'posts/:id', element: <PostEdit /> },
+        { path: 'gallery', element: <AdminGallery /> },
+        { path: 'gallery/:id', element: <GalleryEdit /> },
       ],
     },
-  ]);
+  ])
 }
