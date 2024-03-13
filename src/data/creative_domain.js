28383f09a357975fa76994cfa082199a81761bdb
raimamukherjee29 <raimamukherjee2910@gmail.com>
2024-03-14 03:59:38 +0530

    changed domains layout

---
+++ src/data/creative_domain.js
@@ -0,0 +1,44 @@
+export const creativeDomainData = [
+  {
+    name: 'Photography',
+    image: require('../assets/images/photography.jpeg'),
+    description:
+      'In the realm of photography, the focus is on capturing and preserving special moments. The UiPath community emphasizes an intuitive approach, streamlining photo editing and organization processes to enhance the seamless preservation of cherished memories.',
+    buttonlink: '',
+  },
+  {
+    name: 'Content Writing',
+    image: require('../assets/images/contentwriting.jpg'),
+    description:
+      'In the niche of specialized content creation, the primary objective is to craft compelling narratives. This community centers on intuitive approaches, ensuring streamlined processes for creating engaging content that captivates audiences and communicates ideas effectively.',
+    buttonlink: '',
+  },
+  {
+    name: 'Social Media Handling',
+    image: require('../assets/images/socialmedia.jpeg'),
+    description:
+      'In the arena of social media management, the focus is on curating engaging content. This community emphasizes user-friendly strategies, streamlining processes for efficient scheduling, posting, and interaction to enhance online presence and audience engagement.',
+    buttonlink: '',
+  },
+  {
+    name: 'Event Management',
+    image: require('../assets/images/eventmanagement.avif'),
+    description:
+      'In the domain of event management, the spotlight is on orchestrating memorable experiences. This community prioritizes user-friendly tools, streamlining processes for efficient planning, coordination, and execution to ensure seamless and successful events.',
+    buttonlink: '',
+  },
+  {
+    name: 'Video Editing',
+    image: require('../assets/images/videoedit.jpg'),
+    description:
+      'Within the video editing sphere, the community centers around crafting compelling visual stories. Emphasizing user-friendly interfaces, it streamlines processes for efficient editing, enhancing the creation of engaging videos with seamless transitions and impactful storytelling.',
+    buttonlink: '',
+  },
+  {
+    name: 'Graphic Designing',
+    image: require('../assets/images/graphicdesign.png'),
+    description:
+      'In the graphic design realm, the focus is on crafting visually captivating creations. This community prioritizes intuitive tools, streamlining processes for efficient design, enabling the creation of stunning graphics that effectively communicate ideas and captivate audiences.',
+    buttonlink: '',
+  },
+]
--- src/sections/Home/teachers.jsx
+++ src/sections/Home/teachers.jsx
@@ -1,51 +1,51 @@
-import FICPic from "../../assets/images/facultyImages/Picture Collage Save the Date Card (Instagram Post).jpg";
-import POCPic from "../../assets/images/facultyImages/faculty2.jpg";
+import FICPic from '../../assets/images/facultyImages/Picture Collage Save the Date Card (Instagram Post).jpg'
+import POCPic from '../../assets/images/facultyImages/faculty2.jpg'
 export const FIC = () => {
   return (
-    <div className="h-[250vh] md:h-screen w-full bg-white mb-10">
-      <div className="h-full flex-col justify-center items-center w-full flex md:flex-row">
-        <div className=" h-full w-1/2 p-4">
-          <span className="font-bold text-black w-full h-full flex flex-col justify-center items-center">
-            <span className="text-8xl md:text-9xl py-0 bg-gradient-to-br from-orange-700  via-orange-300 to-orange-700 grad bg-clip-text text-transparent">
+    <div className='h-[250vh] md:h-screen w-full bg-white mb-10'>
+      <div className='h-full flex-col justify-center items-center w-full flex md:flex-row'>
+        <div className=' h-full w-1/2 p-4'>
+          <span className='font-bold text-black w-full h-full flex flex-col justify-center items-center'>
+            <span className='text-8xl md:text-9xl py-0 bg-gradient-to-br from-orange-700  via-orange-300 to-orange-700 grad bg-clip-text text-transparent'>
               UiPath
             </span>
-            <span className="text-2xl py-0 text-center w-[400px]">
+            <span className='text-2xl py-0 text-center w-[400px]'>
               Student Community KIIT
             </span>
             <span></span>
           </span>
         </div>
-        <div className=" h-full md:py-36 md:pr-20 w-1/2 gap-8 flex md:flex-row flex-col">
-          <div className="h-full w-full md:w-1/2 mb-52">
+        <div className=' h-full md:py-36 md:pr-20 w-1/2 gap-8 flex md:flex-row flex-col'>
+          <div className='h-full w-full md:w-1/2 mb-52'>
             <img
               src={FICPic}
-              className="h-full object-cover w-full"
-              alt="faculty incharge"
+              className='h-full object-cover w-full'
+              alt='faculty incharge'
             />
-            <h1 className=" text-center font-bold text-xl leading-8 font-oswald">
+            <h1 className=' text-center font-bold text-xl leading-8 font-oswald'>
               FACULTY IN-CHARGE
             </h1>
-            <h2 className=" text-center font-bold mb-2">Subhra Debdas</h2>
-            <p className=" text-center font-normal">
+            <h2 className=' text-center font-bold mb-2'>Dr. Subhra Debdas</h2>
+            <p className=' text-center font-normal'>
               Associate Professor and Associate Dean University PG Program,
               <br />
               School of Electrical Engineering,
               <br /> KIIT Deemed to be University
             </p>
           </div>
-          <div className="h-full w-full  md:w-1/2 mb-52">
+          <div className='h-full w-full  md:w-1/2 mb-52'>
             <img
-              src="https://res.cloudinary.com/dy0raom2p/image/upload/v1710142817/yqkmibtiz6zslkq8rdkv.jpg"
-              alt="POC"
-              className="h-full object-cover w-full"
+              src='https://res.cloudinary.com/dy0raom2p/image/upload/v1710142817/yqkmibtiz6zslkq8rdkv.jpg'
+              alt='POC'
+              className='h-full object-cover w-full'
             />
-            <h1 className=" text-center font-bold text-xl leading-8 font-oswald">
+            <h1 className=' text-center font-bold text-xl leading-8 font-oswald'>
               POINT OF CONTACT
             </h1>
-            <h2 className=" text-center font-bold mb-2">
+            <h2 className=' text-center font-bold mb-2'>
               Prof.(Dr.) Abhishek Ray
             </h2>
-            <p className=" text-center font-normal">
+            <p className=' text-center font-normal'>
               Professor & Dean (Industry Engagements),
               <br /> School of Computer Engineering,
               <br /> KIIT Deemed to be University
@@ -54,5 +54,5 @@ export const FIC = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
